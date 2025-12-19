740e8d05728a54b1afaffe1d1112ba2f7a69551b
my282 <fm282@DESKTOP-FLQPLN7.localdomain>
2025-12-19 11:46:56 +0900

    change : use cpp format

--- original_tools/h2d_src/main.cpp
+++ original_tools/h2d_src/main.cpp
@@ -1,32 +1,53 @@
-#include <cstddef>
-#include <cstdlib>
 #include <iostream>
-
+#include <sstream>
+#include <string>
 using namespace std;
-
+void print_bin(long int n) {
+  cout << "bin:";
+  for (int i = 63; i > 0; --i) {
+    if ((n >> i) & 1) {
+      cout << "1";
+    } else {
+      cout << "0";
+    }
+  }
+  cout << endl;
+}
 int main(int argc, char *argv[]) {
   long int user_input = 0;
-  char *endp = NULL;
   if (argc < 2) {
-    cout << "usage: ./h2d <hex or decimal>" << endl;
+    cout << "usage : h2d <dec or hex>" << endl;
     return 1;
   }
-  if (argv[1][0] == '0' && argv[1][1] == 'x') {
-    user_input = strtol(argv[1], NULL, 16);
-    cout << user_input << endl;
-  } else {
-    user_input = strtol(argv[1], &endp, 10);
-    if (*endp != '\0') {
+  string input = argv[1];
+  // 16進数の判定
+  if (input.size() > 2 && input.substr(0, 2) == "0x") {
+    istringstream ss1(input);
+    ss1 >> hex >> user_input;
 
-      user_input = strtol(argv[1], &endp, 16);
-      if (*endp != '\0') {
-        cout << "usage: ./h2d <hex or decimal>" << endl;
-        return 1;
-      }
+    if (!ss1.fail() && ss1.eof()) {
       cout << user_input << endl;
+      print_bin(user_input);
       return 0;
     }
-    cout << "0x" << hex << user_input << endl;
+  } else {
+    istringstream ss2(input);
+    ss2 >> dec >> user_input;
+    if (!ss2.fail() && ss2.eof()) {
+      cout << hex << "0x" << user_input << endl;
+      print_bin(user_input);
+      return 0;
+    }
   }
-  return 0;
+
+  istringstream ss3(input);
+  ss3 >> hex >> user_input;
+  if (!ss3.fail() && ss3.eof()) {
+    cout << user_input << endl;
+    print_bin(user_input);
+    return 0;
+  }
+
+  cout << "usage : h2d <dec or hex>" << endl;
+  return 1;
 }
